9c1e45eb126efead7b01ef413bfa3d0a2f96614f
cyyang <yueyangc67@gmail.com>
2025-10-20 21:25:01 +0800

    finish linear embedding rmsnorm

--- cs336_basics/models.py
+++ cs336_basics/models.py
@@ -2,11 +2,44 @@ import torch
 import torch.nn as nn
 from einops import rearrange, einsum
 
-class CyyLinear(nn.Module):
-    def __init__(self, in_features, out_features):
-        super(CyyLinear, self).__init__()
-        w = nn.Parameter(torch.randn(out_features, in_features))
-        b = nn.Parameter(torch.randn(out_features))
+class LinearModule(nn.Module):
+    def __init__(self, in_features, out_features, device=None, dtype=None):
+        super(LinearModule, self).__init__()
+        self.device = device if device is not None else torch.device("cpu")
+        self.dtype = dtype if dtype is not None else torch.float32
+        self.weight = nn.Parameter(torch.randn(out_features, in_features, dtype=self.dtype).to(self.device), requires_grad=True)
+        std = 2 / (in_features + out_features)
+        nn.init.trunc_normal_(self.weight.data, std=std, a=-3*std, b=3*std)
 
     def forward(self, x):
-        return x
+        return x @ self.weight.t()
+
+class EmbeddingModule(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
+        """
+        num_embeddings: 词汇表大小
+        embedding_dim: 嵌入向量的维度
+        """
+        super(EmbeddingModule, self).__init__()
+        self.device = device if device is not None else torch.device("cpu")
+        self.dtype = dtype if dtype is not None else torch.float32
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+
+        self.embedding_matrix = nn.Parameter(torch.randn(num_embeddings, embedding_dim).to(self.device), requires_grad=True)
+        nn.init.trunc_normal_(self.embedding_matrix.data, mean=0, std=1.0, a=-3*1.0, b=3*1.0)
+
+    def forward(self, x):
+        return self.embedding_matrix[x]
+
+class RmsNormModule(nn.Module):
+    def __init__(self, d_model, eps=1e-5, device=None, dtype=None):
+        super(RmsNormModule, self).__init__()
+        self.device = device if device is not None else torch.device("cpu")
+        self.dtype = dtype if dtype is not None else torch.float32
+        self.d_model = d_model
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(d_model, dtype=self.dtype, device=self.device).to(self.device), requires_grad=True)
+    def forward(self, x):
+        return (self.alpha * x / torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + self.eps)).to(x.dtype)
+
