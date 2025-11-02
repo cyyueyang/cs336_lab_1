ef0a7341178c0f4bb7874b79ba5ad0bc2d418723
cyyang <yueyangc67@gmail.com>
2025-11-02 21:10:32 +0800

    finish transformer model!

--- cs336_basics/models.py
+++ cs336_basics/models.py
@@ -1,8 +1,10 @@
+import json
 import torch
 import torch.nn as nn
 from einops import rearrange, einsum
 from typing import List, Tuple, Optional
 import math
+import os
 
 class LinearModule(nn.Module):
     def __init__(self, in_features, out_features, device=None, dtype=None):
@@ -160,9 +162,121 @@ class TranformerBlock(nn.Module):
         max_seq_len = 2048
         assert d_model % num_heads == 0
         self.d_k = d_model // num_heads
-        self.pos_encoder = RotaryPositionalEmbedding(theta=10000, d_k=int(self.d_k), max_seq_len=max_seq_len, device=self.device)
+        self.attn = CausalMultiHeadSelfAttention(d_model, num_heads, device=self.device, dtype=self.dtype)
+        self.ffn = SwiGLUModule(d_model, d_ff, device=self.device, dtype=self.dtype)
+        self.rmsnorm1 = RmsNormModule(d_model, d_ff, device=self.device, dtype=self.dtype)
+        self.rmsnorm2 = RmsNormModule(d_model, d_ff, device=self.device, dtype=self.dtype)
 
     def forward(self, x):
+        attn_output = self.attn(self.rmsnorm1(x))
+        x = x + attn_output
+        ffn_output = self.ffn(self.rmsnorm2(x))
+        x = x + ffn_output
+        return x
+
+class BasicTransformerLM(nn.Module):
+    def __init__(self, vocab_size, context_length, num_layers, d_model, d_ff, num_heads, device=None, dtype=None):
+        super(BasicTransformerLM, self).__init__()
+        self.vocab_size = vocab_size
+        self.context_length = context_length
+        self.num_layers = num_layers
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.num_heads = num_heads
+
+        assert d_model % num_heads == 0
+
+        self.device = device if device is not None else torch.device("cpu")
+        self.dtype = dtype if dtype is not None else torch.float32
+
+        self.token_embedding = EmbeddingModule(self.vocab_size, self.d_model, device=self.device, dtype=self.dtype)
+        self.layers = nn.ModuleList(
+            [
+                TranformerBlock(self.d_model, self.d_ff, self.num_heads, self.device, self.dtype)
+                for _ in range(self.num_layers)
+            ]
+        )
+        self.final_norm = RmsNormModule(d_model, d_ff, device=self.device, dtype=self.dtype)
+        self.lm_head = LinearModule(d_model, vocab_size, device=self.device, dtype=self.dtype)
+
+    def get_num_params(self, non_embedding=True):
+        num_params = [p.numel() for p in self.parameters()]
+        if non_embedding:
+            num_params -= self.lm_head.weight.numel()
+        return num_params
+
+
+    def forward(self, x: torch.Tensor):
+        batch_size, seq_len = x.shape
+        x = self.token_embedding(x)
+        for layer in self.layers:
+            x = layer(x)
+        x = self.final_norm(x)
+        x = self.lm_head(x)
+        return x
+
+    @torch.no_grad()
+    def generate(self,
+                 x: torch.Tensor,
+                 max_new_tokens: int,
+                 temperature: float = 1.0,
+                 top_k: int | None = None,
+                 eos_token_id: int | None = None):
+
+        if x.dim() == 1:
+            x = x.unsqueeze(0)
+
+        original_seq_len = x.size(-1)
+
+        for _ in range(max_new_tokens):
+            # 截断 保留最大的上下文长度
+            x = x[:, -self.context_length:] if x.size(1) > self.context_length else x
+            # [batch, seq_len, vocab_size]
+            logits = self.forward(x)
+
+            next_token_logits = logits[:, -1]
+            temperature_scaled_next_token_logits = next_token_logits / temperature
+
+            if top_k is not None:
+                topk_values, _ = torch.topk(
+                    temperature_scaled_next_token_logits,
+                    min(top_k, next_token_logits.size(-1)),
+                    sorted=True
+                )
+
+                threshold = topk_values[:, -1]
+                topk_mask = temperature_scaled_next_token_logits < threshold
+                temperature_scaled_next_token_logits.masked_fill_(topk_mask, float("-inf"))
+
+            next_token_probabilities = softmax(temperature_scaled_next_token_logits, dim=-1)
+            next_token_id = torch.multinomial(next_token_probabilities, 1)
+            if eos_token_id is not None and next_token_id.item() == eos_token_id:
+                break
+
+            x = torch.cat([x, next_token_id], dim=-1)
+        new_token_ids = x[: original_seq_len:]
+        return new_token_ids
+    @classmethod
+    def from_pretrained(cls, pretrained_model_path):
+        config_path = os.path.join(pretrained_model_path, "model_config.json")
+        with open(config_path, "r", encoding="utf-8") as f:
+            config = json.load(f)
+            model = cls(**config)
+            weights_path = os.path.join(pretrained_model_path, "model.pt")
+            state_dict = torch.load(weights_path)
+
+            unwanted_prefix = "_orig_mod."
+
+            for key, value in list(state_dict.items()):
+                if key.startswith(unwanted_prefix):
+                    state_dict[key[len(unwanted_prefix):]] = state_dict.pop(key)
+
+                model.load_state_dict(state_dict)
+            return model
+
+
+
+
 
 
 
